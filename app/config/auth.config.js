e49df71e117e7d4d47856b5618db5b705c7c8138
ExRonin <palvido81@gmail.com>
2022-09-17 07:28:15 +0700

    v1

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "prafit-secret-key"
+};
---
+++ app/models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
+  })
+
+);
+
+module.exports = User;
+
+const Schema = mongoose.Schema;
+const tokenSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+    expires: 900,
+  },
+});
+
+module.exports = mongoose.model("Token", tokenSchema);
